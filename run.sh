62fbbb1dad505b1c84cd987fd1ac9c2c81ca86e7
blackmanjaro <lw2957409438@gmail.com>
2022-03-01 15:35:16 +0800

    change update time

--- run.sh
+++ run.sh
@@ -12,8 +12,19 @@ cp -rf /usr/lib/libboost_*
 cp -rf /usr/include/boost
 
 mkdir -p /boost
+
+if [ -d "/boost/include" ]; then
+    rm -rf /boost/include
+fi
+
 mkdir -p /boost/include
+
+if [ -d "/boost/lib" ]; then
+    rm -rf /boost/lib
+fi
+
 mkdir -p /boost/lib
+
 cp -rf /usr/lib/libboost_* /boost/lib
 cp -rf /usr/include/boost  /boost/include/
 
