e18b18fef03bc616897485453ea2efefc6fd610b
blackmanjaro <lw2957409438@gmail.com>
2022-03-01 10:59:55 +0800

    test docker image id

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,4 +13,5 @@ ls -al
 echo "Hello $1"
 #time=$(date)
 id=`cat /proc/self/cgroup | grep -o -e "docker/.*"| head -n 1 |sed "s/docker\/\(.*\)/\\1/"`
+echo $(cat /proc/self/cgroup | grep -o -e "docker/.*"| head -n 1 |sed "s/docker\/\(.*\)/\\1/")
 echo "::set-output name=time::$id"
