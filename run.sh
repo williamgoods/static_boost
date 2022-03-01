5820f67ca851759e65618d272d1fd1b1b3fbaa66
blackwilliam <lw2957409438@gmail.com>
2022-03-01 15:08:26 +0800

    test boost directory

--- run.sh
+++ run.sh
@@ -8,4 +8,13 @@ pacman -S --noconfirm extra/boost
 ls /usr/lib/libboost_*
 ls /usr/include/boost
 
+cp -rf /usr/lib/libboost_* 
+cp -rf /usr/include/boost
+
+mkdir -p /boost
+mkdir -p /boost/include
+mkdir -p /boost/lib
+cp -rf /usr/lib/libboost_* /boost/lib
+cp -rf /usr/include/boost  /boost/include/
+
 echo "we show leave archlinux continer"
